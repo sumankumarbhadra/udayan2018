e30177baa3bbe97008efdeda758970432c3c1409
Udit Narayan Sia <udit.sia@gmail.com>
2018-01-26 13:56:17 +0530

    update

--- indexjs.js
+++ indexjs.js
@@ -1,10 +1,11 @@
 //function for document on ready event
+var navWidth;
+var navFlag = true;
 $(document).ready(function(){
     //onclick function for menu nav
     $(".navBtn").click(function(){
-        //cgange icon and hide nav
-        $("#navMenu").toggleClass("hideMenuNav");
-        $("#navDivBtn").toggleClass("hideMenuDiv");
+        //cgange icon and hide n
+        navWidth = document.getElementById("navMenu").width;
         $("#navBtnIcn").toggleClass("fa-angle-double-left fa-angle-double-right");
     });
 });
