0abf4f3d43a8048f1ff8a33538f38494ea12ad4f
Udit Narayan Sia <udit.sia@gmail.com>
2018-01-26 14:13:26 +0530

    drone position fixed

--- indexjs.js
+++ indexjs.js
@@ -1,14 +1,6 @@
 //function for document on ready event
 var navWidth;
 var navFlag = true;
-$(document).ready(function(){
-    //onclick function for menu nav
-    $(".navBtn").click(function(){
-        //cgange icon and hide n
-        navWidth = document.getElementById("navMenu").width;
-        $("#navBtnIcn").toggleClass("fa-angle-double-left fa-angle-double-right");
-    });
-
 //drone
 var $p1 = $("#l-prop1");
 var $p2 = $("#l-prop2");
