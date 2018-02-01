cc14ce3771aaa909c725a3de6945fd3623908984
Udit Narayan Sia <udit.sia@gmail.com>
2018-02-01 15:36:13 +0530

    nav close btn and animation added

--- mobile-site/mobileScript.js
+++ mobile-site/mobileScript.js
@@ -4,11 +4,16 @@ function showNav() {
 
     if (navFlag === false) {
         document.querySelector("#mainNav").style.height = "100%";
+        document.querySelector("#mNavBtn").style.transform="translateX(-100px)";
+        document.querySelector("#mNavBtn2").style.top="15px";
+
         return navFlag = true;
 
     }else  if(navFlag === true)
         {
             document.querySelector("#mainNav").style.height = "0%";
+            document.querySelector("#mNavBtn").style.transform="translateX(15px)";
+            document.querySelector("#mNavBtn2").style.top="-100px";
             return navFlag = false;
         }
 }
