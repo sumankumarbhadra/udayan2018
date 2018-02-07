83336f861eabc0374c8016791c28764a43a99fe7
Adarsh Mohanty <adarsh23m@gmail.com>
2018-02-07 16:09:25 +0530

    fixed mobile event pages

--- mobile-site/contest/js/custom.js
+++ mobile-site/contest/js/custom.js
@@ -380,3 +380,30 @@ $(document).on('ready', function () {
     };
 
 });
+/*navbar js*/
+
+
+/*Nav Start*/
+var navFlag = false;
+function showNav() {
+
+    if (navFlag === false) {
+        document.querySelector("#mainNav").style.width = "100%";
+        document.querySelector("#mNavBtn").style.transform="translateX(-100px)";
+        document.querySelector("#mNavBtn2").style.top="15px";
+
+        return navFlag = true;
+
+    }else  if(navFlag === true)
+    {
+        document.querySelector("#mainNav").style.width = "0%";
+        document.querySelector("#mNavBtn").style.transform="translateX(15px)";
+        document.querySelector("#mNavBtn2").style.top="-100px";
+        return navFlag = false;
+    }
+}
+
+
+  
+/*Nav End*/
+
